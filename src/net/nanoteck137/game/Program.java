acfe488490e79526bc900d12f3126c17454dabb7
Nanoteck137 <patrik.millvik@gmail.com>
2018-09-29 22:16:55 +0200

    Added a window class

--- src/net/nanoteck137/game/Program.java
+++ src/net/nanoteck137/game/Program.java
@@ -1,21 +1,13 @@
 package net.nanoteck137.game;
 
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
 import static org.lwjgl.opengl.GL11.*;
 
-import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
 
 public class Program {
 	
-	private long window;
-	
-	private Input input;
+	private Window window;
 	
 	public Program() {		
 		init();
@@ -23,51 +15,7 @@ public class Program {
 	}
 	
 	private void init() {
-		if(!glfwInit()) {
-			System.err.println("Error initalizing GLFW");
-			System.exit(-1);
-		}
-		
-		window = glfwCreateWindow(800, 800, "Hello World", 0, 0);
-		if(window == NULL) {
-			System.err.println("Error creating the window");
-			System.exit(-1);
-		}
-		
-		input = new Input();
-		
-		glfwSetKeyCallback(window, new GLFWKeyCallback() {
-			public void invoke(long window, int key, int scancode, int action, int mods) {
-				input.setKeyState(key, action != GLFW_RELEASE);
-			}
-		});
-		
-		glfwSetMouseButtonCallback(window, new GLFWMouseButtonCallback() {
-			public void invoke(long window, int button, int action, int mods) {
-				input.setButtonState(button, action != GLFW_RELEASE);
-			}
-		});
-		
-		try ( MemoryStack stack = stackPush() ) {
-			IntBuffer pWidth = stack.mallocInt(1); // int*
-			IntBuffer pHeight = stack.mallocInt(1); // int*
-
-			// Get the window size passed to glfwCreateWindow
-			glfwGetWindowSize(window, pWidth, pHeight);
-
-			// Get the resolution of the primary monitor
-			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-			// Center the window
-			glfwSetWindowPos(
-				window,
-				(vidmode.width() - pWidth.get(0)) / 2,
-				(vidmode.height() - pHeight.get(0)) / 2
-			);
-		}
-		
-		glfwMakeContextCurrent(window);
-		
+		window = new Window("Hello World", 800, 800);
 	}
 	
 	private void loop() {
@@ -84,22 +32,13 @@ public class Program {
 			glfwPollEvents();
 			glClear(GL_COLOR_BUFFER_BIT);
 
-			if(input.isButtonPressed(0)) {
+			if(window.getInput().isKeyPressed(GLFW_KEY_W)) {
 				System.out.println("Hello World");
 			}
 			
-			glBegin(GL_LINE);
+			window.update();
 			
-			glVertex2f(0, 0);
-			glVertex2f(0, 3);
-			
-			glEnd();
-			
-			glfwSwapBuffers(window);
-			
-			input.update();
-			
-			if(glfwWindowShouldClose(window)) {
+			if(window.closed()) {
 				running = false;
 			}
 		}
---
+++ src/net/nanoteck137/game/Window.java
@@ -0,0 +1,92 @@
+package net.nanoteck137.game;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryStack.*;
+
+import java.nio.*;
+
+import org.lwjgl.glfw.*;
+import org.lwjgl.system.*;
+
+public class Window {
+
+	private long window;
+	
+	private int width;
+	private int height;
+	
+	private Input input;
+	
+	public Window(String title, int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+		if(!glfwInit()) {
+			System.err.println("Error initalizing GLFW");
+			System.exit(-1);
+		}
+		
+		input = new Input();
+		
+		window = glfwCreateWindow(width, height, title, 0, 0);
+		if(window == 0) {
+			System.err.println("Error creating the window");
+			System.exit(-1);
+		}
+		
+		glfwSetKeyCallback(window, new GLFWKeyCallback() {
+			public void invoke(long window, int key, int scancode, int action, int mods) {
+				input.setKeyState(key, action != GLFW_RELEASE);
+			}
+		});
+		
+		glfwSetMouseButtonCallback(window, new GLFWMouseButtonCallback() {
+			public void invoke(long window, int button, int action, int mods) {
+				input.setButtonState(button, action != GLFW_RELEASE);
+			}
+		});
+		
+		try ( MemoryStack stack = stackPush() ) {
+			IntBuffer pWidth = stack.mallocInt(1); // int*
+			IntBuffer pHeight = stack.mallocInt(1); // int*
+
+			// Get the window size passed to glfwCreateWindow
+			glfwGetWindowSize(window, pWidth, pHeight);
+
+			// Get the resolution of the primary monitor
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+			// Center the window
+			glfwSetWindowPos(
+				window,
+				(vidmode.width() - pWidth.get(0)) / 2,
+				(vidmode.height() - pHeight.get(0)) / 2
+			);
+		}
+		
+		glfwMakeContextCurrent(window);
+	}
+	
+	public void update() {
+
+		glfwSwapBuffers(window);
+		input.update();
+	}
+	
+	public boolean closed() {
+		return glfwWindowShouldClose(window);
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public Input getInput() {
+		return input;
+	}
+	
+}
